c14ee76351e8f94af2a69508087342ad8a5a96d4
Alima07 <alex.lima_dev@outlook.com>
2022-09-12 11:33:40 -0300

    Configuracao app.js Servidor

--- app.js
+++ app.js
@@ -1,1 +1,52 @@
+
+
+
+// Requizicao do modulo NPM Express
+const express = require('express');
+
+// Requizicao do Método Multer para trabalhar com processamento de arquivos do lado do servidor
 const multer = require('multer');
+
+// Confiuracao do modulo express para chamar como função
+const app = express();
+
+// Requizicao do modulo NPM Path
+const path = require('path');
+
+// Setando o motor de visualizacao como extensao EJS
+app.set("view engine", 'ejs');
+
+// Receberá as informaçes do formulário e deixara disponivel como um objeto literal
+// Isso fará com que o processamento das informacoes enviadas via formulário pelo método 
+// POST funcione dentro de um objeto literal, assim dando a possibilidade de trabalhar com 
+// esses dados, caso nao possua essas duas linhas de código abaixo nao ira funcionar.
+app.use(express.json());
+app.use(express.urlencoded({ extended: false}));
+
+// Os métodos CRUD, PUT e DELETE nao sao suportados nativamente por todos os navegadores
+// As duas linhas de código abaixo chama o módulo para substituir o metodo POST por PUT ou DELETE,
+// nos formulários, fazendo com que consigamos trabalhar com métodos citados.
+const methodOverride = require('method-override');
+app.use(methodOverride('_method'));
+
+// Configuração para uso como pasta estatica a pasta public
+app.use(express.static(path.join(__dirname, 'public')));
+
+//.................................................................................
+
+//BLOCO PARA USO DE ROTAS DO PROJETO (REQUISICAO ROTEADOR E INDICACAO CAMINHO)
+
+// Router Index
+const IndexRouter = require ('./routers/IndexRouter');
+app.use('/', IndexRouter);
+
+//.................................................................................
+
+// Caso não seja encontrado nenhuma das páginas acima, aplicar redirecionamento para erro 404
+app.use((req, res, next) => {res.status(404).render('not-found.ejs') });
+
+
+// Configurando porta para inicializacao do servidor (Running port 3000)
+app.listen(3000, ()=> {console.log('Server Runing Port 3000')});
+
+
